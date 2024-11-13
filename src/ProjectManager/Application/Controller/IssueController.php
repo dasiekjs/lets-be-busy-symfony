58cf0bd23f2871bc41511ecfd5cabf71a66c23ee
Adam Wolak <adamwolak0501@gmail.com>
2024-11-13 21:07:25 +0100

    WIP: Add Swagger for api

--- src/ProjectManager/Application/Controller/IssueController.php
+++ src/ProjectManager/Application/Controller/IssueController.php
@@ -4,19 +4,34 @@ declare(strict_types=1);
 
 namespace LetsBeBusy\ProjectManager\Application\Controller;
 
-use LetsBeBusy\ProjectManager\Application\DTO\CreateIssueDTO;
 use LetsBeBusy\ProjectManager\Application\DTO\IssueDTO;
 use LetsBeBusy\ProjectManager\Domain\IssueId;
 use LetsBeBusy\ProjectManager\Domain\Repository\IssueRepository;
+use Nelmio\ApiDocBundle\Attribute\Model;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use OpenApi\Attributes as OA;
 
 #[Route('/issue', name: 'app_issue')]
+#[OA\Tag(name: 'Issue', description: 'Manage Issues')]
 class IssueController extends AbstractController
 {
 
     #[Route('/{id}', name: '_issue_show', methods: ['GET'])]
+    #[OA\Get(
+        description: 'Get a single issue',
+        summary: 'Get a single issue',
+    )]
+    #[OA\Response(
+        response: 200,
+        description: 'Return selected Issue',
+        content: new Model(type: IssueDTO::class)
+    )]
+    #[OA\Response(
+        response: 404,
+        description: 'Issue not found'
+    )]
     public function getIssueById(
         string $id,
         IssueRepository $issueRepository,
--- src/ProjectManager/Application/Controller/ProjectController.php
+++ src/ProjectManager/Application/Controller/ProjectController.php
@@ -5,22 +5,39 @@ declare(strict_types=1);
 namespace LetsBeBusy\ProjectManager\Application\Controller;
 
 use LetsBeBusy\ProjectManager\Application\DTO\CreateIssueDTO;
+use LetsBeBusy\ProjectManager\Application\DTO\IssueDTO;
 use LetsBeBusy\ProjectManager\Application\DTO\IssuePreviewDTO;
 use LetsBeBusy\ProjectManager\Application\DTO\ProjectDTO;
 use LetsBeBusy\ProjectManager\Domain\ProjectId;
 use LetsBeBusy\ProjectManager\Domain\Repository\IssueRepository;
 use LetsBeBusy\ProjectManager\Domain\Repository\ProjectRepository;
 use LetsBeBusy\ProjectManager\Domain\Service\IssueService;
+use Nelmio\ApiDocBundle\Attribute\Model;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\Routing\Attribute\Route;
+use OpenApi\Attributes as OA;
 
 #[Route('/project', name: 'app_project')]
+#[OA\Tag(name: 'Projects', description: 'Manage Projects')]
 class ProjectController extends AbstractController
 {
 
     #[Route('/{id}', name: '_find', methods: ['GET'])]
+    #[OA\Get(
+        description: 'Get a single project',
+        summary: 'Get a single project',
+    )]
+    #[OA\Response(
+        response: 200,
+        description: 'Return selected Project',
+        content: new Model(type: ProjectDTO::class)
+    )]
+    #[OA\Response(
+        response: 404,
+        description: 'Project not found'
+    )]
     public function getProjectById(string $id, ProjectRepository $projectRepository): Response
     {
         $project = $projectRepository->getById(new ProjectId($id));
@@ -35,12 +52,29 @@ class ProjectController extends AbstractController
     }
 
     #[Route('/{id}/issues', name: '_find_issues', methods: ['GET'])]
+    #[OA\Get(
+        description: 'Return all issues for project',
+        summary: 'Get all issues for project',
+    )]
+    #[OA\Response(
+        response: 200,
+        description: 'Return issues for selected Project',
+        content: new OA\JsonContent(
+            type: 'array',
+            items: new OA\Items(ref: new Model(type: IssuePreviewDTO::class))
+        )
+    )]
+    #[OA\Response(
+        response: 404,
+        description: 'Project not found'
+    )]
     public function getProjectIssuesById(
         string $id,
         ProjectRepository $projectRepository,
         IssueRepository $issueRepository,
     ): Response
     {
+        // TODO Move this into parameter resolver
         $project = $projectRepository->getById(new ProjectId($id));
 
         if ($project === null) {
@@ -57,6 +91,24 @@ class ProjectController extends AbstractController
     }
 
     #[Route('/{id}/create', name: '_create_issue', methods: ['POST'])]
+    #[OA\Post(
+        description: 'Create a new issue for project',
+        summary: 'Create a new issue for project',
+    )]
+    #[OA\Response(
+        response: 200,
+        description: 'Issue was created',
+        content: new OA\JsonContent(
+            properties: [
+                new OA\Property(property: 'issueId', title: 'Issue ID'),
+            ],
+            type: 'object'
+        )
+    )]
+    #[OA\Response(
+        response: 404,
+        description: 'Project not found'
+    )]
     public function createIssueForProject(
         string $id,
         #[MapRequestPayload] CreateIssueDTO $createIssue,
